96301c7aa95d1456035bf2bb70a70d9c48b36de6
Heigo Kure <heigo.kure@khk.ee>
2015-09-22 11:07:30 +0300

    Minust pilt [Delivers #102576906]

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <link type="text/css" rel="stylesheet" href="style.css"/><!--Sellega võtab kasutusele CSS-i -->
     <title>Avaleht</title>
 </head>
-<h1 align="center" ><a href="http://localhost:63342/Hoidla/index.php" a>Janre Pressi Koduleht</a></h1>
+<h1 align="center" ><a href="index.php" a>Janre Pressi Koduleht</a></h1>
 <a href="https://www.facebook.com/janre.press?fref=ts" a>
     <img id="hed" src="https://scontent.xx.fbcdn.net/hprofile-xtp1/v/t1.0-1/11251046_862946020439808_8056596579037004442_n.jpg?oh=a0dba4a169a9508b2299fe15b327c278&oe=566DBD80"/></a>
 
@@ -15,24 +15,24 @@
     <ul id="ull"><!--Algavad listid-->
         <li><p>Huvid</p>
             <ul id="ull1">
-                <li><a href="http://localhost:63342/Hoidla/Janre1.php" a>Arvutid</a></li>
-                <li ><a href="http://localhost:63342/Hoidla/JanreFilm.php" a>Filmide vaatamine</a></li><!--Listid pluss lingid -->
-                <li ><a href="http://localhost:63342/Hoidla/JanreSport.php" a>Sport</a></li>
-                <li ><a href="http://localhost:63342/Hoidla/JanreMuusika.php" a>Räpp muusika</a></li>
+                <li><a href="Janre1.php" a>Arvutid</a></li>
+                <li ><a href="JanreFilm.php" a>Filmide vaatamine</a></li><!--Listid pluss lingid -->
+                <li ><a href="JanreSport.php" a>Sport</a></li>
+                <li ><a href="JanreMuusika.php" a>Räpp muusika</a></li>
             </ul>
         </li>
         <li><p>Koolid kus käinud olen</p>
             <ol>
-                <li><a href="http://localhost:63342/Hoidla/JanrePohikool.php" a>Võru I Põhikool</a></li>
-                <li><a href="http://localhost:63342/Hoidla/JanreGumnaasium.php" a>Võru Kreutzwaldi Gümnaasium</a></li><!--Veel liste -->
-                <li><a href="http://localhost:63342/Hoidla/JanreKutsekas.php" a>Tartu Kutsehariduskeskus</a></li>
+                <li><a href="JanrePohikool.php" a>Võru I Põhikool</a></li>
+                <li><a href="JanreGumnaasium.php" a>Võru Kreutzwaldi Gümnaasium</a></li><!--Veel liste -->
+                <li><a href="JanreKutsekas.php" a>Tartu Kutsehariduskeskus</a></li>
             </ol>
         </li>
         <li><p>Lemmik näitlejad</p>
             <ul id="ull1" ">
-        <li><a href="http://localhost:63342/Hoidla/JanreNaitlejad.php" a>Kevin Hart</a></li>
-        <li><a href="http://localhost:63342/Hoidla/JanreNaitlejad.php" a>Jason Statham</a></li><!--ja isegi veel rohkem liste -->
-        <li><a href="http://localhost:63342/Hoidla/JanreNaitlejad.php" a>Channing Tatum</a></li>
+        <li><a href="JanreNaitlejad.php" a>Kevin Hart</a></li>
+        <li><a href="JanreNaitlejad.php" a>Jason Statham</a></li><!--ja isegi veel rohkem liste -->
+        <li><a href="JanreNaitlejad.php" a>Channing Tatum</a></li>
     </ul>
     </li>
     </ul>
